404dbc4dce828c3812fcbd4d2ad369721af89c81
BrandonArroyo <barroyo50@gmail.com>
2016-02-28 23:08:26 -0600

    set up

---
+++ Instagram/LoginViewController.swift
@@ -0,0 +1,65 @@
+//
+//  LoginViewController.swift
+//  Instagram
+//
+//  Created by Brandon Arroyo on 2/28/16.
+//  Copyright © 2016 Brandon Arroyo. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class LoginViewController: UIViewController {
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func onSignIn(sender: AnyObject) {
+        PFUser.logInWithUsernameInBackground(usernameField.text!, password: passwordField.text!) { (user: PFUser?, error: NSError?) -> Void in
+            if user != nil {
+                print("logged in")
+                self.performSegueWithIdentifier("LoginSegway", sender: nil)
+            }
+            
+        }
+    }
+    @IBAction func onSignUp(sender: AnyObject) {
+        
+        let newUser = PFUser()
+        newUser.username = usernameField.text
+        newUser.password = passwordField.text
+        newUser.signUpInBackgroundWithBlock { (success: Bool, error: NSError?) -> Void in
+            if success{
+                print("we got a user")
+                self.performSegueWithIdentifier("LoginSegway", sender: nil)
+            }else{
+                print(error?.localizedDescription)
+                if error?.code == 202{
+                    print("User Already Exists")
+                }
+            }
+        }
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
